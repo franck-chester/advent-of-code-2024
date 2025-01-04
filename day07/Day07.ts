7815cce5208ee8414a5a2e83fca449d47aed3736
Franck Schmidlin <franck.schmidlin@theverygroup.com>
2025-01-04 09:41:17 +0000

    day 7 part 2

--- day07/Day07.ts
+++ day07/Day07.ts
@@ -14,10 +14,10 @@ part1.inputFile = 'input.txt';
 export function part2(entries: string[], isTest? : boolean, testNumber?:number): string { return part2Implementation(entries, isTest, testNumber); };
 part2.day = day;
 part2.testFile = 'test.txt';    // ['test01.txt'];
-part2.example = '???';          // ['???'];
+part2.example = '11387';          // ['???'];
 part2.inputFile = 'input.txt';
 
-type Operator = "+"|"*";
+type Operator = "+"|"*"|"||";
 
 class Operation extends TreeNode<number, Operator> {
     constructor(parent : TreeRoot<number, Operator>, name: Operator, value: number) {
@@ -79,6 +79,43 @@ function part1Implementation(entries: string[], isTest? : boolean, testNumber?:n
 // ACTUAL CODE - Part TWO  //
 /////////////////////////////
 function part2Implementation(entries: string[], isTest? : boolean, testNumber?:number) {
-    let solution = '???'
+    let solution = 0
+    const m = /(?<result>\d*):\s(?<values>.*)/
+    for(let i=0; i<entries.length; i++){
+        const matches = m.exec(entries[i]);
+        const result = parseInt(matches?.groups?.result!);
+        const values = (matches?.groups?.values!).split(' ').map(s => parseInt(s));
+        console.log(`i = ${i}, result = ${result}, values = ${values}`)
+        let equation : Equation = {
+            result,
+            values,
+            possibleCalculations : new Tree<number, Operator>(values[0])
+        }
+        
+        
+        let nodes = [equation.possibleCalculations.root];
+        for(let j = 1; j<values.length; j++){
+            let nextNodes = new Array<Operation>();
+            const value = values[j];
+            nodes.forEach(n =>{
+                nextNodes.push(n.addChild(new Operation(n, '+', n.value + value)));
+                nextNodes.push(n.addChild(new Operation(n, '*', n.value * value)));
+                nextNodes.push(n.addChild(new Operation(n, '||', parseInt(`${n.value}${value}`))));
+            });
+            nodes = nextNodes;
+        }
+        let validEquations = nodes.filter(n => {
+            if(n.value == result){
+                console.log(n.debugLabel);
+            }
+            return n.value == result
+    });
+        if(validEquations.length > 0){
+            console.log(`valid   : ${entries[i]}`);
+            solution += result;
+        }else {
+            console.error(`invalid : ${entries[i]}`);
+        }
+    }
     return `${solution}`;
 }
